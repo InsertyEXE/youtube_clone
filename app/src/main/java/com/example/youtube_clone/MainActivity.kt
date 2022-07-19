089a91aa50c2ee19624fec33ed915a88bf1a6231
InsertyEXE <insertyend@gmail.com>
2022-07-19 18:11:25 -0300

    feat: Animation fade in and out
    the fade in and out on view video_detail has implemented

--- app/src/main/java/com/example/youtube_clone/MainActivity.kt
+++ app/src/main/java/com/example/youtube_clone/MainActivity.kt
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.View
+import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.youtube_clone.databinding.ActivityMainBinding
 import com.example.youtube_clone.model.ListVideo
 import com.example.youtube_clone.model.Video
 import com.example.youtube_clone.model.VideoAdapter
 import com.google.gson.GsonBuilder
+import kotlinx.android.synthetic.main.video_detail.*
 import kotlinx.coroutines.*
 import okhttp3.OkHttpClient
 import okhttp3.Request
@@ -28,9 +30,11 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbarMain)
         supportActionBar?.title = ""
 
+        view_layer.alpha = 0f
+
         val list = mutableListOf<Video>()
         videoAdapter = VideoAdapter(list){ video ->
-
+                showOverLayer(video)
         }
 
         binding.apply {
@@ -53,6 +57,53 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+
+
+
+    }
+
+    private fun showOverLayer(video: Video) {
+
+       view_layer.animate().apply {
+           duration = 400
+           alpha(0.5f)
+       }
+
+        binding.motionContainer.setTransitionListener(object: MotionLayout.TransitionListener{
+            override fun onTransitionStarted(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int
+            ) {
+
+            }
+
+            override fun onTransitionChange(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int,
+                progress: Float
+            ) {
+                if (progress> 0.5f)
+                    view_layer.alpha = 1.0f - progress
+                else
+                    view_layer.alpha = 0.5f
+            }
+
+            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+
+            }
+
+            override fun onTransitionTrigger(
+                motionLayout: MotionLayout?,
+                triggerId: Int,
+                positive: Boolean,
+                progress: Float
+            ) {
+
+            }
+
+        })
 
     }
 
